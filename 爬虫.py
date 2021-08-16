808734ac3a38507afe66479c4083490f954f3545
黄闻天 HwangWentian <85811921+HwangWentian@users.noreply.github.com>
2021-08-16 13:03:50 +0800

    Add files via upload

---
+++ 爬虫.py
@@ -0,0 +1,70 @@
+import jieba as jb
+from requests import get
+from bs4 import BeautifulSoup as Bs
+from wordcloud import WordCloud as Wc
+
+agents = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 '
+    'OPR/26.0.1656.60',
+    'Opera/8.0 (Windows NT 5.1; U; en)',
+    'Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0',
+    'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2 ',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 '
+    'Safari/534.16',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 '
+    'Safari/536.11',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 '
+    'LBBROWSER',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)',
+    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X '
+    'MetaSr 1.0',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; SE 2.X '
+    'MetaSr 1.0) ',
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; "
+    ".NET CLR 3.0.04506)",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 "
+    "Safari/535.20"]
+
+words = ""
+if __name__ == "__main__":
+    while True:
+        film_id = input("输入该电影在豆瓣上的 ID：")
+        print("开始爬取（此操作可能需要一些时间）")
+        header = {"user-agent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)"}
+        page = get("https://movie.douban.com/subject/%s" % film_id, headers=header).text
+        page = Bs(page, "html5lib")
+
+        name = page.findAll("title")[0].string.strip()
+        if name == "页面不存在":
+            print("错误的 ID")
+            continue
+
+        for i in range(20):
+            url = "https://movie.douban.com/subject/%s/comments?start=%d" % (film_id, i * 20)
+            headers = {"user-agent": agents[i]}
+            page = get(url, headers=headers)
+            page = page.text
+            page = Bs(page, "html5lib")
+            page = page.findAll(class_="short")
+            for t in page:
+                words += t.string
+
+        with open("%s 评论.txt" % name, "w", encoding="utf-8") as file_obj:
+            file_obj.write(words)
+
+        words = "".join(jb.lcut(words))
+
+        cloud = Wc(background_color="white", width=2000, height=2000,
+                   font_path="C:/Windows/Fonts/STZHONGS.TTF")  # 字体文件路径可能需要修改
+        cloud.generate(words)
+        cloud.to_file("%s 评论词云.webp" % name)
+
+        print("Finished.")
